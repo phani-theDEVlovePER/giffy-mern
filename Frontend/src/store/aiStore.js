da9ffe0a9ca0be710ba29c14c188a44e292b6b5c
Krishna Praveen Madepalli <ramcharanphani@gmail.com>
2025-05-02 16:25:08 +0530

    url updated in aiStore.js

--- Frontend/src/store/aiStore.js
+++ Frontend/src/store/aiStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand"
 import axios from "axios"
 
-const API_URL = "http://localhost:5000/api"
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api"
 axios.defaults.withCredentials = true
 
 export const useAiStore = create((set) => ({
@@ -9,12 +9,12 @@ export const useAiStore = create((set) => ({
     isLoading: false,
     error: null,
 
-    heart: async(PROMPT)=>{
-        set({isLoading: true, error: null})
-        try{
-            const response = await axios.post(`${API_URL}/heart`, {PROMPT})
-            set({data: response.data, isLoading: false})
-        }catch(error){
+    heart: async (PROMPT) => {
+        set({ isLoading: true, error: null })
+        try {
+            const response = await axios.post(`${API_URL}/heart`, { PROMPT })
+            set({ data: response.data, isLoading: false })
+        } catch (error) {
             set({ error: error.response.data.message || "Error Fetching Prompt", isLoading: false })
             throw error
         }
