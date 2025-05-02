08719f8f2315535f8d9a9a6cdcb30b8d6992edf9
Krishna Praveen Madepalli <ramcharanphani@gmail.com>
2025-05-02 16:10:24 +0530

    first edits

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,8 @@ import router from "./auth.routes.js"
 import path from "path"
 
 const APP = express()
-APP.use(cors({ origin: "http://localhost:5173", credentials: true }))
+const CLIENT_URL = process.env.CLIENT_URL
+APP.use(cors({ origin: CLIENT_URL, credentials: true }))
 dotenv.config()
 const PORT = process.env.PORT || 5000
 const __dirname = path.resolve()
@@ -18,4 +19,5 @@ if (process.env.NODE_ENV === "production") {
 }
 APP.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
+    console.log(CLIENT_URL)
 })
